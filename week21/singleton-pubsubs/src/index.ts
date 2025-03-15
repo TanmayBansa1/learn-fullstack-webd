1a3a49392a792fdf6332e72911149fc435d96884
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-03-15 12:44:49 +0530

    Add files via upload

--- week21/singleton-pubsubs/src/index.ts
+++ week21/singleton-pubsubs/src/index.ts
@@ -1,10 +1,27 @@
-import { startLogger } from "./logger"
-import { gameManager } from "./store"
+/////// 1)
+//for the game, unuderstanding singleton pattern
 
-startLogger();
+// import { startLogger } from "./logger"
+// import { gameManager } from "./store"
+
+// startLogger();
+// setInterval(() => {
+//     gameManager.addGame(Math.random().toString());
+// }, 5000)
+// create a ws server here to maintain a connection with the client
+
+
+
+/////////// 2)
+//for the pubsub, understanding singleton pattern
+
+import { pubsubManager } from "./pubsubmanager";
+
 setInterval(() => {
-    gameManager.addGame(Math.random().toString());
+    pubsubManager.userSubscribe(Math.random().toString(), "AAPL");
 }, 5000)
 
 
-// create a ws server here to maintain a connection with the client
+setInterval(() => {
+    pubsubManager.relay("AAPL", "100");
+}, 5000)
